3e2bcd854b2019831de424927f1daa485d6d37e6
Mark Dionisius Alvin Putra Gau <160519758+markavin@users.noreply.github.com>
2025-06-10 22:07:06 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // ⬅️ tambahkan ini
   plugins: [react()],
   server: {
     port: 3000,
