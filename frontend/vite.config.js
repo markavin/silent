965e7edf1bb77d4885b331d86320b3328671d637
Mark Dionisius Alvin Putra Gau <160519758+markavin@users.noreply.github.com>
2025-06-10 21:56:22 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,20 @@
-server: {
-  proxy: {
-    '/api': 'http://localhost:5000'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: true,
+    strictPort: true,
+  },
+  preview: {
+    port: 3000,
+    host: true,
+    strictPort: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
   }
-}
+})
